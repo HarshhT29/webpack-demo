81585683cd8c439135e17e3f5ec163709b1a1c42
Harsh Tiwari <harshstudy29@gmail.com>
2025-11-23 22:35:31 +0530

    feat: refactor component to include button and print function, add print.js module

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './wp-icon.png';
+ import _ from 'lodash';
+import printMe from './print.js';
 
-function component() {
-  const element = document.createElement('div');
-  
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+ function component() {
+   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
-document.body.appendChild(component());
+  element.appendChild(btn);
+
+   return element;
+ }
+
+ document.body.appendChild(component());
