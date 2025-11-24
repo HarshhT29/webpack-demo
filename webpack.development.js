b553561266295b97f043fe5751f0c22fd5cfa419
HarshhT29 <132380397+HarshhT29@users.noreply.github.com>
2025-11-25 02:12:57 +0530

    chore: explored dev related options (#2)
    * feat: add development build configuration with webpack
    
    * feat: add watch mode for development build in package.json
    
    * feat: add webpack-dev-server and dev:start script for improved development experience
    
    - Updated package.json to include webpack-dev-server and added a new script for starting the development server.
    - Configured webpack.development.js to serve static files from the dist directory and added runtime chunk optimization.
    
    * chore: Add express and webpack-dev-middleware to devDependencies; set publicPath in webpack configuration
    
    * feat: add server.js to set up express with webpack-dev-middleware for development

---
+++ webpack.development.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Development",
+    }),
+  ],
+};
